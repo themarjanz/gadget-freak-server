861a88cad5d39b32ebc51508f5e12cc920a6ff4f
themarjanz <themarjanz@gmail.com>
2022-10-10 09:25:58 +0600

    added projerct

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 var cors = require('cors')
 app.use(cors())
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 app.use(express.json())
 const port = 5000
@@ -16,13 +17,62 @@ async function run() {
         await client.connect();
         console.log('db connected');
         const productCollection = client.db("gadgetFreak").collection("products");
+        const orderCollection = client.db("gadgetFreak").collection("orders");
 
+        // for json web token
+        app.post("/login", (req, res) => {
+            const email = req.body;
+            // console.log(email);
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+
+            res.send({ token })
+        })
 
         app.post("/uploadPd", async (req, res) => {
             const product = req.body;
-            // console.log(product);
-            const result = await productCollection.insertOne(product);
-            res.send({ success: 'Product Upload Succsessfully' })
+
+            const tokenInfo = req.headers.authorization;
+            // console.log(tokenInfo);
+            const [email, accessToken] = tokenInfo.split(" ")
+            // verifu email call
+            const decoded = verifyToken(accessToken)
+
+            if (email === decoded.email) {
+                const result = await productCollection.insertOne(product);
+                res.send({ success: 'Product Upload Succsessfully' })
+            }
+            else {
+                res.send({ success: "UnAuthoraized Access" })
+            }
+
+        })
+
+        app.get("/products", async (req, res) => {
+            const products = await productCollection.find({}).toArray();
+            res.send(products);
+
+        })
+
+        app.post("/addOrder", async (req, res) => {
+            const orderInfo = req.body;
+            const result = await orderCollection.insertOne(orderInfo);
+            res.send({ success: 'Order Compelete' })
+        })
+
+        app.get("/orderList", async (req, res) => {
+            const tokenInfo = req.headers.authorization;
+            console.log(tokenInfo);
+            const [email, accessToken] = tokenInfo.split(" ")
+            // verifu email call
+            const decoded = verifyToken(accessToken)
+
+            if (email === decoded.email) {
+                const orders = await orderCollection.find({ email: email }).toArray();
+                res.send(orders);
+            }
+            else {
+                res.send({ success: "UnAuthoraized Access" })
+            }
 
         })
     } finally {
@@ -40,3 +90,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// verify token function
+function verifyToken(token) {
+    let email;
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            email = 'Invalid email'
+        }
+        if (decoded) {
+            console.log(decoded);
+            email = decoded
+        }
+
+    });
+    return email;
+}
