ec84675b040aea1b97622c332938aacdd2d68ce3
themarjanz <themarjanz@gmail.com>
2022-10-17 19:39:24 +0600

    added steps

---
+++ step.js
@@ -0,0 +1,24 @@
+/*** 
+ * MODULE - 75.5
+ * 
+ * Server
+ * --------------------------------
+ * 1. heroku login>click enter>open CLI >click login
+ * 2.heroku create
+ * 3.git push heroku main
+ * 4.go heroku link
+ * 5. heroku Dashboard> project>setting>Reveal Config>env added
+ * 6.go client site >http://localhost:5000/ >changed by > https://ancient-peak-16242.herokuapp.com/
+ * -------------------------------------------
+ * 
+ * 7. client site deploy> firebase hosting>
+ * a. firebase login
+ * b. firebase init
+ * c.answer the questions
+ * d.npm run build
+ * e.firebase deploy
+ * 
+ * 
+ * 
+ * 
+ * **/
